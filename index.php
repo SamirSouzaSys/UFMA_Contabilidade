1910090748d4f2ec9c80c039f544e58ad72423e0
SamirSouza <samir.guitar@gmail.com>
2016-04-04 07:46:58 -0300

    Montando fase 2

--- index.php
+++ index.php
@@ -3,61 +3,226 @@ require_once('config.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <?php
 include $head;
 ?>
-
 <body>
     <div id="wrapper">
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Contabilidade - Computação UFMA 2015.1</a>
+      <!-- Navigation -->
+      <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Contabilidade - Computação UFMA 2015.1</a>
+      </div>
+      <!-- /.navbar-header -->
+      <?php
+      include $templates;
+      ?>
+      <!-- /.navbar-static-side -->
+  </nav>
+  <!-- Page Content -->
+  <div id="page-wrapper">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">Introdução</h1>
+          <div>
+            <p>Neste trabalho vamos discorrer sobre tr&ecirc;s redu&ccedil;&otilde;es de valor de ativos, em virtude de desgaste pelo uso, a&ccedil;&atilde;o da natureza, supera&ccedil;&atilde;o tecnol&oacute;gica, exerc&iacute;cio de direitos, ou esgotamento de reservas minerais/ florestais.</p>
+            <p>S&atilde;o elas a Deprecia&ccedil;&atilde;o, a Amortiza&ccedil;&atilde;o e a Exaust&atilde;o.</p>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Conteúdo
             </div>
-            <!-- /.navbar-header -->
-
-            <?php
-            include $templates;
-            ?>
-
-            <!-- /.navbar-static-side -->
-        </nav>
-
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Introdução</h1>
-                        <div>
-                            <p>Neste trabalho vamos discorrer sobre tr&ecirc;s redu&ccedil;&otilde;es de valor de ativos, em virtude</p>
-                            <p>de desgaste pelo uso, a&ccedil;&atilde;o da natureza, supera&ccedil;&atilde;o tecnol&oacute;gica, exerc&iacute;cio de</p>
-                            <p>direitos, ou esgotamento de reservas minerais/ florestais. S&atilde;o elas a Deprecia&ccedil;&atilde;o, a</p>
-                            <p>Amortiza&ccedil;&atilde;o e a Exaust&atilde;o.</p>
+            <!-- .panel-heading -->
+            <div class="panel-body">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" aria-expanded="false" class="collapsed">CONCEITO</a>
+                            </h4>
                         </div>
                     </div>
-                    <!-- /.col-lg-12 -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed" aria-expanded="false">DEPRECIAÇÃO</a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                            <div class="panel-body">
+                                <ol>
+                                    <li><p>Lançamento Contábil</p>
+                                    </li>
+                                    <li><p>Vedações a Depreciação – Legislação IR</p>
+                                    </li>
+                                    <li><p>Inicio da Depreciação</p>
+                                    </li>
+                                    <li>
+                                      <p>
+                                        Conceitos Importantes</p>
+                                    </li>
+                                    <li>
+                                      <p>
+                                        Critério Contábil </p>
+                                    </li>
+                                    <li>
+                                      <p>
+                                        Taxas de Depreciação</p>
+                                    </li>
+                                    <li>
+                                      <p>
+                                        Elementos que integram o custo do imobilizado</p>
+                                        <p>2.8. CPC 27 – Um caso especial</p>
+                                        <ol>
+                                            <ol>
+                                                <li>
+                                                    <p>Métodos de Depreciação</p>
+                                                </li>
+                                                <li>
+                                                    <p>Depreciação de bens adquiridos usados</p>
+                                                </li>
+                                                <li>
+                                                    <p>Baixa do Ativo Não Circulante</p>
+                                                </li>
+                                                <li>
+                                                    <p>CPC 27 – Imobilizado</p>
+                                                </li>
+                                            </ol>
+                                        </ol>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed" aria-expanded="false">Collapsible Group Item #3</a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                            <div class="panel-body">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!-- /.row -->
             </div>
-            <!-- /.container-fluid -->
+            <!-- .panel-body -->
         </div>
-        <!-- /#page-wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
-
+        <ol>
+            <li>
+              <p></p>
+          </li>
+          <li>
+            <p>DEPRECIAÇÃO</p>
+            <ol>
+                <li><p>Lançamento Contábil</p>
+                </li>
+                <li><p>Vedações a Depreciação – Legislação IR</p>
+                </li>
+                <li><p>Inicio da Depreciação</p>
+                </li>
+                <li>
+                  <p>
+                    Conceitos Importantes</p>
+                </li>
+                <li>
+                  <p>
+                    Critério Contábil </p>
+                </li>
+                <li>
+                  <p>
+                    Taxas de Depreciação</p>
+                </li>
+                <li>
+                  <p>
+                    Elementos que integram o custo do imobilizado</p>
+                    <p>2.8. CPC 27 – Um caso especial</p>
+                    <ol>
+                        <ol>
+                            <li>
+                                <p>Métodos de Depreciação</p>
+                            </li>
+                            <li>
+                                <p>Depreciação de bens adquiridos usados</p>
+                            </li>
+                            <li>
+                                <p>Baixa do Ativo Não Circulante</p>
+                            </li>
+                            <li>
+                                <p>CPC 27 – Imobilizado</p>
+                            </li>
+                        </ol>
+                    </ol>
+                </li>
+            </ol>
+        </li>
+        <li>
+          <p>
+            AMORTIZAÇÃO
+        </p>
+        <ol id="l5">
+            <li>
+              <p>
+                Bens Intangíveis sujeitos a Amortização
+            </p>
+        </li>
+        <li>
+          <p>
+            Taxa de Amortização
+        </p>
+    </li>
+    <li>
+      <p>
+        Lançamento Contábil
+    </p>
+</li>
+</ol>
+</li>
+<li>
+  <p>
+    EXAUSTÃO
+</p>
+<ol id="l6">
+    <li>
+      <p>
+        Não deve sofrer exaustão
+    </p>
+</li>
+<li>
+  <p>Recursos Minerais</p>
+</li>
+<li>
+  <p>Recursos Florestais</p>
+</li>
+<li>
+  <p>Lançamento Contábil</p>
+</li>
+</ol>
+</li>
+</ol>
+</div>
+<!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+</div>
+<!-- /#wrapper -->
 <?php
 include $footerScript;
 ?>
-
 </body>
-
 </html>
